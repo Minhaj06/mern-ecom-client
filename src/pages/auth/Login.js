0a4b0d6cc1663ad1af2d06e5138746abdd7e5b1c
Minhaj06 <milonmdminhajkobir@gmail.com>
2023-01-25 14:39:36 +0600

    initial commit

--- src/pages/auth/Login.js
+++ src/pages/auth/Login.js
@@ -1,10 +1,84 @@
 import React from "react";
 import Jumbotron from "../../components/cards/Jumbotron";
+import axios from "axios";
+import toast from "react-hot-toast";
+import { useAuth } from "../../context/auth";
+import { useNavigate, useLocation } from "react-router-dom";
 
 function Login() {
+  // state
+  const [email, setEmail] = useState("minhaj@gmail.com");
+  const [password, setPassword] = useState("Minhaj736#");
+
+  // hook
+  const [auth, setAuth] = useAuth();
+  const navigate = useNavigate();
+  const location = useLocation();
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    try {
+      const { data } = await axios.post(`/login`, {
+        email,
+        password,
+      });
+      console.log(data);
+
+      if (data?.error) {
+        toast.error(data.error);
+      } else {
+        localStorage.setItem("auth", JSON.stringify(data));
+        setAuth({ ...auth, token: data.token, user: data.user });
+        toast.success("Login successful");
+
+        setTimeout(
+          navigate(
+            location.state || `/dashboard/${data?.user?.rolw === 1 ? "admin" : "user"}`
+          ),
+          500
+        );
+      }
+    } catch (err) {
+      console.log(err);
+      toast.error("Login failde. Try again.");
+    }
+  };
+
   return (
     <div>
       <Jumbotron title="Login" />
+
+      <div className="container mt-5">
+        <div className="row">
+          <div className="col-md-6 offset-md-3">
+            <form onSubmit={handleSubmit}>
+              <input
+                type="email"
+                name=""
+                id=""
+                className="form-control mb-4 p-2"
+                placeholder="Enter your email"
+                value={email}
+                onChange={(e) => setEmail(e.target.value)}
+              />
+
+              <input
+                type="password"
+                name=""
+                id=""
+                className="form-control mb-4 p-2"
+                placeholder="Enter your password"
+                value={password}
+                onChange={(e) => setPassword(e.target.value)}
+              />
+
+              <button type="submit" className="btn btn-info">
+                Submit
+              </button>
+            </form>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
